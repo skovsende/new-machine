73cc85981feaca2a5a5851a8f1a59da21dadcc50
Thomas Skovsende <skovsende@gmail.com>
2020-03-13 11:18:48 +0100

    Say yes when installing

--- init-machine.ps1
+++ init-machine.ps1
@@ -1,14 +1,14 @@
 #install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install git
-choco install dotnetcore
-choco install dotnetcore-sdk
-choco install poshgit
-choco install jetbrainstoolbox
-choco install firefox
-choco install vscode
-choco install totalcommander
-choco install slack
-choco install docker-desktop
-choco install putty
+choco install git -y
+choco install dotnetcore -y
+choco install dotnetcore-sdk -y
+choco install poshgit -y
+choco install jetbrainstoolbox -y
+choco install firefox -y
+choco install vscode -y
+choco install totalcommander -y
+choco install slack -y
+choco install docker-desktop -y
+choco install putty -y
