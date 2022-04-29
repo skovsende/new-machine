3fb779ce65e16314de8227d0aa04e39bd5e9fb62
Thomas Skovsende <skovsende@gmail.com>
2022-04-29 13:56:58 +0200

    Update init-machine.ps1

--- init-machine.ps1
+++ init-machine.ps1
@@ -1,12 +1,15 @@
 #install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+choco install grep
+choco install sed
 choco install git -y
 choco install dotnetcore -y
 choco install dotnet-sdk -y
 choco install dotnet -y
 choco install dotnetcore-sdk -y
 choco install poshgit -y
+choco install oh-my-posh
 choco install jetbrainstoolbox -y
 choco install firefox -y
 choco install vscode -y
@@ -15,3 +18,4 @@ choco install slack -y
 choco install putty -y
 choco install kdiff3 -y
 choco install terraform -y
+choco install firacode -y
